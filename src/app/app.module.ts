72b261b3df802745bb39ffee62c8019c691966ec
Nico Schmidt <nicoolschmidt@live.de>
2020-02-10 21:03:41 +0100

    Add component node-graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NodeGraphComponent } from './components/node-graph/node-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodeGraphComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/node-graph/node-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node-graph',
+  templateUrl: './node-graph.component.html',
+  styleUrls: ['./node-graph.component.sass']
+})
+export class NodeGraphComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
